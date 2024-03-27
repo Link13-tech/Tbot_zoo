42adf1b1656d1224859fbcb77497f1a7cc840516
Link13-tech <151656048+Link13-tech@users.noreply.github.com>
2024-03-27 17:53:58 +0300

    working code 13.01b

---
+++ bot_main.py
@@ -0,0 +1,122 @@
+import asyncio
+from aiogram.enums import ParseMode
+import logging
+import sys
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from aiogram import F
+from aiogram.utils.formatting import (Bold, as_list, as_marked_section)
+from victory_handler import router
+from data import TOKEN, answers
+from aiogram.types import ReplyKeyboardRemove
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+dp = Dispatcher()
+dp.include_router(router)
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    kb = [
+        [
+            types.KeyboardButton(text="Команды"),
+            types.KeyboardButton(text="Контакты"),
+            types.KeyboardButton(text="Описание бота"),
+        ],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+    )
+
+    kb_inline = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="Да", callback_data="victory")]])
+
+    await message.answer(
+        f"Привет *{message.from_user.full_name}!* Рад видеть! У меня есть для тебя увлекательная игра. Давай \
+проверим, какое татемное животное сответствует твоему характеру! Просто отвечай на мои вопросы, и мы узнаем \
+какой зверь подходит тебе, а может быть и птица, но не будем загадывать. Готов начать приключение?☺",
+        parse_mode=ParseMode.MARKDOWN, reply_markup=kb_inline
+    )
+    await message.answer("Или можешь ознакомиться с моими функциями и командами подробнее☺", reply_markup=keyboard)
+
+
+@dp.message(F.text.lower() == "описание бота")
+async def description_button(message: types.Message):
+    await description_command(message)
+
+
+@dp.message(F.text.lower() == "контакты")
+async def contact_button(message: types.Message):
+    await contact_command(message)
+
+
+@dp.message((F.text.lower() == "/description" or F.text.lower() == "описание бота"))
+async def description_command(message: types.Message):
+    if message.text.lower() == "/description":
+        await message.answer(
+            "Это бот-викторина, помогающая пользователю подобрать тотемное животное в формате развлечения. \
+            А также рассказать посетителям канала о программе опеки",
+            reply_markup=ReplyKeyboardRemove())
+    else:
+        await message.answer("Это бот-викторина, помогающая пользователю подобрать тотемное животное в формате \
+развлечения. \nА также рассказать посетителям канала о программе опеки.")
+
+
+@dp.message(F.text.lower() == "/contact" or F.text.lower() == "контакты")
+async def contact_command(message: types.Message):
+    if message.text.lower() == "/contact":
+        await message.answer(
+            "Если у вас возникли вопросы вы можете\nсвязаться с нами:\nE-mail: bac9.91@mail.ru\n"
+            "Телефон: 8(999)960-05-80\nУзнать подробнее о программе опеки по адресу:\n"
+            "https://www.moscowzoo.ru/my-zoo/become-a-guardian/",
+            reply_markup=ReplyKeyboardRemove()
+        )
+    else:
+        await message.answer("Если у вас возникли вопросы вы можете\nсвязаться с нами:\nE-mail: bac9.91@mail.ru\n"
+                             "Телефон: 8(999)960-05-80\nУзнать подробнее о программе опеки по адресу:\n"
+                             "https://www.moscowzoo.ru/my-zoo/become-a-guardian/")
+
+
+@dp.message(F.text.lower() == "/feedback")
+async def send_feedback_request(message: types.Message):
+    await message.answer(
+        'Пожалуйста, отправьте ваш отзыв о боте\nВ формате "/feedback - ваш отзыв ".',
+        reply_markup=ReplyKeyboardRemove()
+    )
+
+
+@dp.message(F.text.lower() == "команды")
+async def commands(message: types.Message):
+    response = as_list(
+        as_marked_section(
+            Bold("Команды:"),
+            "/victory - запуск викторины",
+                "/contact - контактная информация",
+                "/description - описание бота",
+                "/feedback - оставить отзыв",
+            marker="✅ ",
+        ),
+    )
+    await message.answer(
+        **response.as_kwargs()
+    )
+
+
+@dp.message(
+    ~F.text.in_(answers), ~F.text.lower().startswith("/feedback"),
+    ~F.text.lower().startswith("/start"), ~F.text.lower().startswith("/contact"),
+    ~F.text.lower().startswith("/description"), ~F.text.lower().startswith("/victory")
+)
+async def handle_invalid_message(message: types.Message):
+    await message.answer("Простите, я не понимаю вашего сообщения. Пожалуйста, используйте доступные команды.")
+
+
+async def main() -> None:
+    bot = Bot(TOKEN)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
---
+++ victory_handler.py
@@ -0,0 +1,174 @@
+import os
+import random
+import smtplib
+from email.message import EmailMessage
+from typing import Type
+
+from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
+from aiogram import Router, types, F
+from data import questions, responses, weights, description_animals, animals
+from aiogram.types import ReplyKeyboardRemove, InlineKeyboardButton, InlineKeyboardMarkup
+
+router = Router()
+user_answers = {}
+
+
+@router.message(Command("victory"))
+async def victory_question(message: types.Message, state=FSMContext):
+    current_question_key = (list(questions.keys())[0])
+    await state.set_state(current_question_key)
+    await message.answer(responses["start"])
+    await ask_question(message, questions["Q1"])
+
+
+@router.callback_query(lambda query: query.data == "victory")
+async def start_victory_callback(callback_query: types.CallbackQuery, state: FSMContext):
+    await callback_query.answer()
+    await callback_query.message.delete()
+    current_question_key = (list(questions.keys())[0])
+    await state.set_state(current_question_key)
+    await callback_query.message.answer(responses["start"])
+    await ask_question(callback_query.message, questions["Q1"])
+
+
+@router.message(Command("feedback"))
+async def handle_feedback(message: types.Message):
+    if message.content_type == types.ContentType.TEXT:
+        feed = message.text[len("/feedback"):].strip()
+        user_id = message.from_user.id
+        user_name = message.from_user.full_name
+
+        await save_feedback_to_file(user_id, user_name, feed)
+        await message.answer("Спасибо за ваш отзыв!")
+    else:
+        await message.answer("Пожалуйста, отправьте текстовое сообщение после команды /feedback.")
+
+
+async def save_feedback_to_file(user_id: int, user_name: str, feedback: str):
+    with open('feedback.txt', 'a', encoding='utf-8') as file:
+        file.write(f"User ID: {user_id}, User name: {user_name}, Feedback: {feedback}\n")
+
+
+async def ask_question(message: types.Message, question_text: str):
+    kb = [[types.KeyboardButton(text="Да"), types.KeyboardButton(text="Возможно"), types.KeyboardButton(text="Нет")]]
+    keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
+    await message.answer(question_text, reply_markup=keyboard)
+
+
+@router.message(lambda message: message.text in ["Да", "Возможно", "Нет"])
+async def answer_question_and_send_result(message: types.Message, state: FSMContext):
+    current_question_key = await state.get_state()
+    user_answer = message.text
+    user_answers[current_question_key] = user_answer
+    next_question_key = f"Q{int(current_question_key[1:]) + 1}"
+
+    if next_question_key in questions:
+        await state.set_state(next_question_key)
+        await message.answer(responses[next_question_key])
+        await ask_question(message, questions[next_question_key])
+    else:
+        result_message = await analyze_answers(state)
+        await send_result(message, state)
+        await guarding(message)
+        user_name = message.from_user.full_name
+        await send_email(user_name, animals[f"{result_message}"])
+
+
+async def analyze_answers(state: FSMContext):
+    def analyze(answers):
+        return {animal: weight_fn(answers) for animal, weight_fn in weights.items()}
+
+    result = analyze(user_answers)
+    print(result)
+    sorted_result = {k: v for k, v in sorted(result.items(), key=lambda item: item[1], reverse=True)}
+    max_weight = max(sorted_result.values())
+    max_weight_animals = [animal for animal, weight in sorted_result.items() if weight == max_weight]
+    selected_animal = random.choice(max_weight_animals)
+    await state.set_data({'selected_animal': selected_animal})
+    return selected_animal
+
+
+@router.message(lambda message: message.text.lower() in ["конечно", "может быть позже"])
+async def guarding(message: types.Message):
+    if message.text.lower() == "конечно":
+        current_directory = os.getcwd()
+        video_path = os.path.join(current_directory, "video", "опека.mp4")
+        await message.answer_video(video=types.FSInputFile(video_path), reply_markup=ReplyKeyboardRemove())
+    elif message.text.lower() == "может быть позже":
+        await message.answer("Спасибо, что приняли участие☺", reply_markup=ReplyKeyboardRemove())
+
+
+@router.message(F.text.lower() == "попробовать ещё раз?")
+async def restart_victory(message: types.Message, state: Type[FSMContext]):
+    await victory_question(message, state)
+
+
+@router.callback_query(lambda query: query.data == "share")
+async def share_results(callback_query: types.CallbackQuery, state: FSMContext):
+    data = await state.get_data()
+    selected_animal = data.get('selected_animal')
+    photo_animals_path = os.path.join(os.getcwd(), "photo_animals", f"{selected_animal}.jpg")
+    share_message = f"Друзья я прошёл викторину! Моё тотемное животное: {animals[selected_animal]}.\
+Присоединяйтесь к викторине в нашем боте: [https://t.me/ZooVictory_bot]."
+
+    vk_share_link = f"https://vk.com/share.php?url=https://t.me/ZooVictory_bot&title={share_message}"
+
+    await callback_query.answer()
+    await callback_query.message.answer_photo(
+        photo=types.FSInputFile(photo_animals_path),
+        caption=share_message,
+        reply_markup=InlineKeyboardMarkup(
+            inline_keyboard=[[InlineKeyboardButton(text="Поделиться", url=vk_share_link)]]),
+    )
+
+
+@router.message()
+async def send_result(message: types.Message, state: FSMContext):
+    data = await state.get_data()
+    result_message = data.get('selected_animal')
+    current_directory = os.getcwd()
+    photo_animals_path = os.path.join(current_directory, "photo_animals", f"{result_message}.jpg")
+
+    kb = [
+        [types.KeyboardButton(text="Конечно"), types.KeyboardButton(text="Может быть позже")],
+        [types.KeyboardButton(text="Попробовать ещё раз?")]
+    ]
+    keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
+
+    kb_inline = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="Поделиться результатом",
+                                                                            callback_data="share")]])
+
+    caption = f"Поздравляю, это твой тотем☺! {description_animals[result_message]}\n\n"
+
+    await message.answer_photo(
+        caption=caption,
+        photo=types.FSInputFile(photo_animals_path),
+        reply_markup=kb_inline
+    )
+    await message.answer("Кстати, ты можешь стать его опекуном, хочешь узнать больше?", reply_markup=keyboard)
+
+
+@router.message()
+async def send_email(user_name, selected_animal):
+    smtp_host = 'smtp.mail.ru'
+    smtp_port = 587
+    email_login = 'bac9.91@mail.ru'
+    email_password = '9UauvSHFzP5Ub8Y1KQcD'
+
+    recipient_email = 'bac9.91@mail.ru'
+
+    message = EmailMessage()
+    message['From'] = email_login
+    message['To'] = recipient_email
+    message['Subject'] = 'Результаты викторины'
+    message_text = f'Пользователь: {user_name} - Тотемное животное: {selected_animal}'
+    message.set_content(message_text)
+    try:
+        with smtplib.SMTP(smtp_host, smtp_port) as smtp_server:
+            smtp_server.starttls()
+            smtp_server.login(email_login, email_password)
+            smtp_server.send_message(message)
+        print('Письмо успешно отправлено')
+    except Exception as e:
+        print('Ошибка при отправке письма:', e)
